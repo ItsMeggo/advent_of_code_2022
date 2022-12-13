7d07f8874f7defc7d80575ec3a899de0690bc33e
Meggo <105523419+ItsMeggo@users.noreply.github.com>
2022-12-13 18:43:50 +0000

    Update main.py

--- day 1/main.py
+++ day 1/main.py
@@ -17,5 +17,29 @@ def find_highest_calories():
 
     return highest_calories
 
-answer = find_highest_calories()
-print(f"highest calories is {answer}")
+def top_3_elves():
+    top_calories =[0,0,0]
+    current_calories = 0
+    for item in calories_list:
+
+        if item == "":
+            current_calories = 0
+
+        else:
+            current_calories += int(item)
+
+        if current_calories > min(top_calories):
+            top_calories.sort()
+            top_calories[0] = (current_calories)
+
+    print(top_calories)
+    top_three_total = sum(top_calories)
+
+    return top_three_total
+
+
+highest_calories_answer = find_highest_calories()
+print(f"highest calories is {highest_calories_answer}")
+
+top_3_answer = top_3_elves()
+print(f"Top 3 total is {top_3_answer}")
