b563fe1a08f3d41c0fb846a79c1f65abdf493d98
Meggo <105523419+ItsMeggo@users.noreply.github.com>
2022-12-17 12:36:25 +0000

    Add second part
    first part is more in depth, and second part is simplified to be able to find variable unique string length.

--- day06/main.py
+++ day06/main.py
@@ -1,21 +1,30 @@
 data = open("data.txt").read()
-print(data)
+# print(data)
 
-for letter in range(len(data)):
-    first_letter = data[letter]
-    second_letter = data[letter+1]
-    third_letter = data[letter+2]
-    fourth_letter = data[letter+3]
-
-
-    print(f"first letter is {first_letter}")
-    print(f"second letter is {second_letter}")
-    print(f"third letter is {third_letter}")
-    print(f"fourth letter is {fourth_letter}")
+def find_marker(input):
+    for letter in range(len(input)):
+        first_letter = input[letter]
+        second_letter = input[letter+1]
+        third_letter = input[letter+2]
+        fourth_letter = input[letter+3]
 
 
-    if first_letter != second_letter and first_letter != third_letter and first_letter != fourth_letter:
-        if second_letter != third_letter and second_letter != fourth_letter:
-            if third_letter != fourth_letter:
-                print(f"The code starts at {letter+3}")
-                break
+        # print(f"first letter is {first_letter}")
+        # print(f"second letter is {second_letter}")
+        # print(f"third letter is {third_letter}")
+        # print(f"fourth letter is {fourth_letter}")
+
+
+        if first_letter != second_letter and first_letter != third_letter and first_letter != fourth_letter:
+            if second_letter != third_letter and second_letter != fourth_letter:
+                if third_letter != fourth_letter:
+                    print(f"The code starts at {letter+4}")
+                    break
+
+def find_unique_string(input, number):
+    for letter in range(len(input)):
+        if len(set(data[letter:(letter+number)])) == number:
+            return letter + number
+
+find_marker(data)
+print(find_unique_string(data, 14))
